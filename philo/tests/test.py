da6afcb94132eafbcf5899e3c23cdda4a7e6948d
tmorris <tmorris@student.42.fr>
2021-07-01 12:10:14 +0200

    Add tests for if philos die in time

--- philo/tests/test.py
+++ philo/tests/test.py
@@ -3,6 +3,7 @@ import subprocess
 
 RED = "\033[31m"
 GREEN = "\033[32m"
+WHITE = "\033[37m"
 RESET = "\033[0m"
 
 def test_error_codes():
@@ -12,17 +13,19 @@ def test_error_codes():
             ("./philo 2 2 3 4 5", 0),
             ("./philo 2 2 3 test 5", -1),
             ("./philo 2 -5 2 9", -1),
+            ("./philo 2 -5- 2 9", -1),
+            ("./philo 2 5 +2 9", -1),
+            ("./philo 2 5 2 a", -1),
             ("./philo 2 3 4 5 6 7", -1),
             ]
 
-
-    print("Testing error codes")
+    print(f"{WHITE}Testing error codes{RESET}")
     passed = 0;
     total = 0;
     for cmd in cmds:
         total += 1
         header = f"{cmd[0]}"
-        header += " " * (30 - len(header))
+        header += " " * (25 - len(header))
         header += f"expecting [{cmd[1]}]"
         print(header + "." * (45 - len(header)), end="")
         try:
@@ -41,6 +44,42 @@ def test_error_codes():
                 print(f"{RED}FAILED{RESET} (Returned Error Code)")
     return (passed, total)
 
+def test_dying_philos():
+    base = "./philo "
+    cmds = [
+            ("2 1 200 10", 1),
+            ("2 1000 2 2", 0),
+            ]
+
+    print(f"{WHITE}Testing death conditions{RESET}")
+    passed = 0;
+    total = 0;
+    for cmd in cmds:
+        total += 1
+        header = f"{base}{cmd[0]}"
+        header += " " * (25 - len(header))
+        header += f"expect death"*cmd[1]
+        print(header + "." * (45 - len(header)), end="")
+        try:
+            result = subprocess.check_output(base+cmd[0], shell=True, text=True, timeout=1)
+            if "has died" in result and cmd[1]:
+                print(f"{GREEN}PASSED{RESET} (Death!)")
+                passed += 1
+            elif cmd[1]:
+                print(f"{RED}FAILED{RESET} (No death)")
+            else:
+                print(f"{RED}FAILED{RESET} (Accidental death)")
+        except subprocess.CalledProcessError as e:
+                print(f"{RED}FAILED{RESET} (Returned Error Code)")
+        except subprocess.TimeoutExpired as e:
+            if cmd[1]:
+                print(f"{RED}FAILED{RESET} (no death in time)")
+            else:
+                print(f"{GREEN}PASSED{RESET} (no death in time)")
+                passed += 1
+    return (passed, total)
+
+
 if __name__ == '__main__':
     if not os.path.isdir("logs"):
         try:
@@ -49,9 +88,15 @@ if __name__ == '__main__':
             print("Could not create logs directory")
             exit()
     passed, total = 0, 0
-    
-    new_passed, new_total = test_error_codes()
-    passed += new_passed
-    total += new_total
 
-    print(f"\nScore: {passed}/{total}")
+    test_functions = [
+            test_error_codes,
+            test_dying_philos,
+            ]
+    for test in test_functions:
+        new_passed, new_total = test()
+        passed += new_passed
+        total += new_total
+        print("\n")
+
+    print(f"{WHITE}Score: {GREEN*(passed == total)+RED*(passed != total)}{passed}/{total}{RESET}")
